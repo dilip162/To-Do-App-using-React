f9b412da2925de88454fe58c9b9915cbaf714dbf
dilip <dilipbaghel162@gmail.com>
2022-09-29 13:18:42 +0530

    improving the app UI

--- src/TodoList.js
+++ src/TodoList.js
@@ -28,24 +28,25 @@ function TodoList() {
         setlistData([])
     }
 
+
     return (
         <>
             <div className="container">
-                <div className="header">To-Do List</div>
+                <div className="header">To-Do App</div>
                 <input type='text' placeholder="Add Activity" value={activity} onChange={(e) => setActivity(e.target.value)} />
                 <button onClick={addActivity}>Add</button>
                 <p className="List-heading">Here is your List</p>
                 {listData != [] && listData.map((data, i) => {
                     return (
-                        <>
-                            <p key={i}>
+                        <div className="section-two">
+                            <div className="lists" key={i}>
                                 <div className="listData">{data}</div>
-                                <div className="btn-position"><button onClick={() => removeActivity(i)}>Remove</button></div>
-                            </p>
-                        </>
+                                <div className="btn-position"><button className="btn-remove" onClick={() => removeActivity(i)}>Remove</button></div>
+                            </div>
+                        </div>
                     )
                 })}
-                {listData.length >= 1 && <button onClick={removeAll}>Remove All</button>}
+                {listData.length >= 1 && <button className="removeAll-btn" onClick={removeAll}>Remove All</button>}
 
             </div>
         </>
